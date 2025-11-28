0dbc8192511bdbfba23afbb6a3ee80b8236b5d1d
Etch <acyclicgraphs@gmail.com>
2025-11-28 06:12:26 -0500

    Create LearningSession deployment script
    Added a deployment script for LearningSession contract.

--- script/LearningSession.s.sol
+++ script/LearningSession.s.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "forge-std/Script.sol";
+import {LearningSession} from "../src/LearningSession.sol";
+
+/// @title LearningSessionScript
+/// @notice Simple deployment script for the DED Learning Session showcase contract.
+contract LearningSessionScript is Script {
+    function run() public {
+        vm.startBroadcast();
+        new LearningSession();
+        vm.stopBroadcast();
+    }
+}
